7734b8ec67bd8078d3e7ffba45f0e6913336e008
Bart van Heukelom <mail@bartvh.nl>
2018-05-08 00:13:21 +0200

    uurtarief en dagenmaand verwisseld

--- kosten.py
+++ kosten.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys
+import statistics
 
 import berekening
 
@@ -15,6 +16,8 @@ dagenrange = (4, 20)
 dagenrangerange = range(dagenrange[0], dagenrange[1]+1)
 urenrange = range(dagenrange[0]*8, dagenrange[1]*8+1)
 
+uurtarief_bereik = (40, 80, 140)
+
 
 def simplemain():
     persoonlijke_berekening(12, E(80), True)
@@ -28,32 +31,49 @@ def plotmain():
 
     plt.ylabel('Bedrag')
     plt.xlabel('Dagen per maand')
+    #plt.xlabel('Uurtarief')
     plt.axis([
         *dagenrange,
+        #uurtarief_bereik[0], uurtarief_bereik[2],
         0, 100000
     ])
     plt.xticks(dagenrangerange)
+    #plt.xticks(range(uurtarief_bereik[0], uurtarief_bereik[2]+1, 10))
     plt.yticks(range(0, 100001, 10000))
     plt.grid()
 
     lines = [plt.plot([], [], label=u)[0] for u in berekening.Berekening.uitkomsten()]
     plt.legend()
 
-    slider_uurtarief = Slider(
+    slider1 = Slider(
         plt.axes([0.1, 0.1, 0.8, 0.04]),
         'Uurtarief', 40, 150, 80
+        #'Dagen per maand', dagenrange[0], dagenrange[1], statistics.median(dagenrange)
     )
 
     # for uti in range(60, 125, 10):
     def update(_=None):
-        uurtarief = E(slider_uurtarief.val)
-        x, y = plot_dagenmaand_nettoinkomen(uurtarief)
+
+        uurtarief = E(slider1.val)
+        #dagenmaand = slider1.val
+
+        x = []
+        y = []
+        for urenmaand in urenrange:
+        #for ut in range(uurtarief_bereik[0], uurtarief_bereik[2]+1):
+            dagenmaand = urenmaand / 8
+            #uurtarief = E(ut)
+            res = persoonlijke_berekening(dagenmaand, uurtarief)
+            x.append(dagenmaand)
+            #x.append(uurtarief)
+            y.append(res)
+
         for i, ln in enumerate(lines):
             ln.set_xdata(x)
             ln.set_ydata([e[i] for e in y])
         fig.canvas.draw_idle()
     update()
-    slider_uurtarief.on_changed(update)
+    slider1.on_changed(update)
 
     plt.get_current_fig_manager().window.showMaximized()
     plt.show()
@@ -64,17 +84,6 @@ def persoonlijke_berekening(dagenmaand, uurtarief, printit=False):
         dagenmaand, uurtarief,
         extra_bruto_inkomen=prive.brutoloon_baan(dagenmaand)
     )
-
-
-def plot_dagenmaand_nettoinkomen(uurtarief):
-    x = []
-    y = []
-    for urenmaand in urenrange:
-        dagenmaand = urenmaand / 8
-        res = persoonlijke_berekening(dagenmaand, uurtarief)
-        x.append(dagenmaand)
-        y.append(res)
-    return x, y
 
 
 if __name__ == '__main__':
